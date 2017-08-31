4fe3ebce6c07ccd7f02d6948bcece03ebd1c4864
Gabriel Hamdan <ghamdan.eng@gmail.com>
2017-08-31 20:45:41 -0300

    INIT

---
+++ lib/zodiac_clock.rb
@@ -0,0 +1,5 @@
+require "zodiac_clock/version"
+
+module ZodiacClock
+  # Your code goes here...
+end
---
+++ spec/zodiac_clock_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe ZodiacClock do
+  it "has a version number" do
+    expect(ZodiacClock::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
